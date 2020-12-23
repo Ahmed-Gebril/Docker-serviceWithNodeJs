67dafa40d6e46e6ae7b274c29416a53904f6d789
Ahmed-Gebril <36602022+Ahmed-Gebril@users.noreply.github.com>
2020-12-23 02:12:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello there from Symbio');
+  res.send('Hello there from planet Earth');
 });
 
 app.listen(PORT, HOST);
